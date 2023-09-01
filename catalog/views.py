b91c135653cdc3742c704d1277fcc0efc93fa59f
vitaly <dudkin231@mail.ru>
2023-09-01 13:54:45 +0300

    added changes

--- catalog/views.py
+++ catalog/views.py
@@ -16,8 +16,29 @@ class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
-    success_url = reverse_lazy('catalog:home_page')
+    success_url = reverse_lazy('catalog:create_product')
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            formset = VersionFormset(instance=self.object)
+
+        context_data['formset'] = formset
+        return context_data
+
+    def from_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
 
 class ProductUpdateView(UpdateView):
     model = Product
@@ -45,7 +66,6 @@ class ProductUpdateView(UpdateView):
             formset.save()
 
         return super().form_valid(form)
-
 
     def get_success_url(self):
         return reverse('catalog:update_product', args=[self.kwargs.get('pk')])
