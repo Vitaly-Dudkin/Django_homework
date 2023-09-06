07bf845779ccdfaf2a1f341b356e7b71b9648f89
vitaly <dudkin231@mail.ru>
2023-09-06 12:30:01 +0300

    added changes

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    phone = models.CharField(max_length=35, verbose_name='phone_number', null=True, blank=True)
+    avatar = models.ImageField(upload_to='users/', verbose_name='image', null=True, blank=True)
+    country = models.CharField(max_length=50, verbose_name='Country', null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
